589455eb860263068d066435e37ecb5e6afaaa39
elenamishina <mishina.elena.95@gmail.com>
2025-06-17 23:45:03 +0300

    HW8 is completed

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,41 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("empty", func(t *testing.T) {
+		cmd := []string{}
+		env := make(Environment)
+		retCode := RunCmd(cmd, env)
+		require.Equal(t, 1, retCode)
+	})
+	t.Run("run error", func(t *testing.T) {
+		cmd := []string{"testdata/ech.sh"}
+		env := make(Environment)
+		retCode := RunCmd(cmd, env)
+		require.Equal(t, 1, retCode)
+	})
+	t.Run("command error", func(t *testing.T) {
+		cmd := []string{"ls", "path"}
+		env := make(Environment)
+		retCode := RunCmd(cmd, env)
+		require.Equal(t, 2, retCode)
+	})
+	t.Run("simple without args", func(t *testing.T) {
+		cmd := []string{"testdata/echo.sh"}
+		env := make(Environment)
+		env["BAR"] = EnvValue{Value: "bar", NeedRemove: false}
+		retCode := RunCmd(cmd, env)
+		require.Equal(t, 0, retCode)
+	})
+	t.Run("simple with args", func(t *testing.T) {
+		cmd := []string{"echo", "hello"}
+		env := make(Environment)
+		retCode := RunCmd(cmd, env)
+		require.Equal(t, 0, retCode)
+	})
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: ./go-envdir </path/to/env/dir> <command> [args...]")
+		os.Exit(1)
+	}
+
+	envPath := os.Args[1]
+	command := os.Args[2]
+	args := os.Args[3:]
+
+	env, err := ReadDir(envPath)
+	if err != nil {
+		fmt.Println("Error read env")
+		os.Exit(1)
+	}
+
+	cmd := append([]string{command}, args...)
+	retCode := RunCmd(cmd, env)
+	os.Exit(retCode)
 }
