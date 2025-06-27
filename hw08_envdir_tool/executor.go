c145a002fab0f5fb880be048ef3dc3da8dfaee06
elenamishina <mishina.elena.95@gmail.com>
2025-06-27 22:52:38 +0300

    HW8 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -14,7 +14,7 @@ func RunCmd(cmd []string, env Environment) (returnCode int) {
 		return 1
 	}
 
-	command := exec.Command(cmd[0], cmd[1:]...)
+	command := exec.Command(cmd[0], cmd[1:]...) // #nosec G204
 
 	command.Stdin = os.Stdin
 	command.Stdout = os.Stdout
